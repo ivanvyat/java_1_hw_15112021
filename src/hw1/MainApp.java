c855b5cfdf3d3c5dfe02adddab3377642f5024e9
ivanvyt <9831268766@mail.ru>
2021-11-22 23:28:49 +0700

    Домашняя работа

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,6 +2,11 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
-
+        printThreeWords();
     }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\n" + "Banana\n" + "Apple\n");
+    }
+
 }
